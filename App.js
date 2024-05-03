aa14d687d063c34b32ea903014c411f0a37bf2d9
gs-ssharma <ssharma@gainsight.com>
2024-05-03 21:22:24 +0530

    learnings

--- App.js
+++ App.js
@@ -1,35 +1,70 @@
-const heading = React.createElement("h1", {id: "heading"}, "hello from namastereact");
-const root = ReactDOM.createRoot(document.getElementById("root"))
-console.log(heading);// returns react elements, ie js object
-root.render(heading);
-//{} object uses to create attribute to class
 
-//nested structure
-// const parent = React.createElement("div",{id: "parent"},
-// React.createElement("div",{id: "child"},[React.createElement("h1", {},"i m h1 tag"),React.createElement("h2", {},"i m h2 tag")]
-// ))
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-//Reactelement(object)=html(browser umderstands);
-const parent = React.createElement("div", { id: "parent" }, [
-    React.createElement("div", { id: "child" }, [
-      React.createElement("h1", {}, "I'm an h1 tag"),
-      React.createElement("h2", {}, "I'm an h2 tag")
-    ]),
-    React.createElement("div", { id: "child2" }, [
-      React.createElement("h1", {}, "I'm an h1 tag"),
-      React.createElement("h2", {}, "I'm an h2 tag")
-    ]),
-    React.createElement("div", { id: "child3" }, [
-      React.createElement("h1", {}, "I'm an h1 tag"),
-      React.createElement("h2", {}, "I'm an h2 tag")
-    ]),
-    React.createElement("div", { id: "child4" }, [
-      React.createElement("h1", {}, "I'm an h1 tag"),
-      React.createElement("h2", {}, "I'm an h2 tag")
-    ])
-  ]);
+
   //above code creates complexity for resolve this here is we have JSX
-  root.render(parent);
+  // root.render(parent);
   //npm is repo for managing pacakges almost all.
   //npx parcel index.html[source file]
   //created a server for this above command
+  //npx->executing a paca
+  //npm install react
+  //hmr = hot module replecaement
+  //file watching algorithm
+  //parcel gives faster build because it's caching things also
+  //parcel also does image optimzation,
+  //for production build it also minifies. it also does bundleing
+  //consistent hashing-->parcel
+  //code splitting-->parcel
+  //diagnostis->parcel
+  //better error handling suggestion
+  //also helps to host on https
+  //different build for dev and production build
+  //npx parcel build index.html ->prod build
+  //tree shacking algorithm ->>parcel->i.e remove unused code 
+  //differential building->parcel-> to support old browesers
+
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  //const heading = React.createElement("h1", {id: "heading"}, "this is namste jsx");
+  //jsx
+
+  const jsxHeading = (<h1 className="headsing">
+    this is namste js
+     </h1>);
+  //jsxheading is react elemet that is object.
+
+
+
+//React components.
+//Two types compoents.
+//class based compoents -> old way. uses js class
+//functional based components -> new way uses js functions.
+
+
+//React functional components -> just normal js function, 
+const ele = <span> nnn</span>
+const Titel = (
+ <div>
+   {ele}
+    <h1>namaste title component</h1>
+  </div>
+)
+const number = 1000;
+const HeadingComponent = () =>{
+  return <div>
+    {/* <TitelComponent/> */}
+    {Titel} 
+    
+     {/* here u can run any js code */}
+    <h1>namaste functional component</h1>
+  </div>
+}
+  console.log(jsxHeading);
+  //console.log(heading);
+  // root.render(heading);
+  //root.render(jsxHeading);
+  // you can wrtite {title()}-> same for functionalcomponent
+  root.render(<HeadingComponent/>)
+  root.render(<HeadingComponent></HeadingComponent>)
+ 
